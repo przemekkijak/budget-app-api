58bfd4c75d7af7d34c863d927f139ce666551120
Przemek Kijak <>
2023-05-07 17:14:55 +0200

    importing file

--- BudgetApp.Application/Features/Transactions/Commands/ImportTransactions.cs
+++ BudgetApp.Application/Features/Transactions/Commands/ImportTransactions.cs
@@ -1,16 +1,33 @@
+using System.Dynamic;
+using System.Globalization;
+using CsvHelper;
 using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace BudgetApp.Core.Features.Transactions.Commands;
 
 public class ImportTransactions : IRequest
 {
-    public byte[] FileBytes { get; set; }
+    public IBrowserFile ImportedFile { get; init; }
 }
 
 public class ImportTransactionsHandler : IRequestHandler<ImportTransactions>
 {
-    public Task Handle(ImportTransactions request, CancellationToken cancellationToken)
+    public async Task Handle(ImportTransactions request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        await using var streamData = request.ImportedFile.OpenReadStream(cancellationToken: cancellationToken);
+        using var reader = new StreamReader(streamData);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        await csv.ReadAsync();
+        csv.ReadHeader();
+        var results = new List<ExpandoObject>();
+        
+        while (await csv.ReadAsync())
+        {
+            results.Add(csv.GetRecord<dynamic>());
+        }
+        
+        Console.WriteLine("test");
     }
 }
