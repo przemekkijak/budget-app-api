1f07e9550ab47dbd91acc219cac6eaa86d054fba
Przemek Kijak <>
2023-06-25 17:21:45 +0200

    users controller wip

--- BudgetApp/Controllers/UsersController.cs
+++ BudgetApp/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BudgetApp.Controllers;
 
 [Controller]
+[Route("api/users")]
 public class UsersController : ApiControllerBase
 {
     private readonly IMediator mediator;
